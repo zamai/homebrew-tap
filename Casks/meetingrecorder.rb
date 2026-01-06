d60eb1d86507b5f30e7b53e19db8aeba9422c845
Alex Zamai <zamai@cast.ai>
2026-01-06 15:19:47 +0100

    fix brew download

--- Casks/meetingrecorder.rb
+++ Casks/meetingrecorder.rb
@@ -2,7 +2,7 @@ cask "meetingrecorder" do
   version "0.2"
   sha256 "abc35408d95a9a83a65ca8258d6f04c92b19cd5412459c8e399dd2bff7613ece"
 
-  url "https://github.com/zamai/MeetingRecorder/releases/download/v#{version}/MeetingRecorder-#{version}.zip"
+  url "https://github.com/zamai/MeetingRecorder/releases/download/v#{version}/MeetingRecorder.zip"
   name "MeetingRecorder"
   desc "Record system audio and microphone simultaneously on macOS"
   homepage "https://github.com/zamai/MeetingRecorder"
